b234d6661098ebd0df2364a04e1a1bcd0fbf8159
linzell <linzellart@gmail.com>
2024-02-21 11:34:14 +0100

    ✨ Add @vercel/analytics package and import Analytics component

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -1,6 +1,7 @@
 import clsx from 'clsx';
 import Heading from '@theme/Heading';
 import styles from './styles.module.css';
+import { Analytics } from "@vercel/analytics/react"
 
 const FeatureList = [
   {
@@ -38,6 +39,7 @@ const FeatureList = [
 function Feature({Svg, title, description}) {
   return (
     <div className={clsx('col col--4')}>
+      <Analytics/>
       <div className="text--center">
         <Svg className={styles.featureSvg} role="img" />
       </div>
